2a4dce89c7976c9ebf15c9e9cba515fdc093140b
moist-mason <67809772+moist-mason@users.noreply.github.com>
2023-02-12 16:53:17 -0600

    parse through methods without "func" prefix (ones inherited from non-MC classes) in AddParams
    Signed-off-by: moist-mason <67809772+moist-mason@users.noreply.github.com>

--- buildSrc/src/main/groovy/com/ancientmc/srgconfig/tasks/AddParams.groovy
+++ buildSrc/src/main/groovy/com/ancientmc/srgconfig/tasks/AddParams.groovy
@@ -3,8 +3,12 @@ package com.ancientmc.srgconfig.tasks
 import com.ancientmc.srgconfig.utils.GetParamMap
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 /**
  * Adds parameters to the inputted TSRG file by reading a corresponding, already mapped JAR.
@@ -14,12 +18,17 @@ class AddParams extends DefaultTask {
     @InputFile File jar // remapped jar
     @OutputFile File tsrgP // output TSRGv2 file with parameters.
 
+    // Descriptor regex for methods
+    @Internal Pattern descRgx = Pattern.compile("\\([^)]*\\)[A-Za-z0-9]+")
+
     @TaskAction
     void exec() {
         FileWriter writer = new FileWriter(tsrgP)
         tsrg.text.eachLine { line ->
-            if((line.contains("func_") || line.contains("init>")) && !line.contains("()")) {
-                String[] split = line.split(" ")
+            String[] split = line.split(" ")
+            String methodDesc = split[1]
+            Matcher matcher = descRgx.matcher(methodDesc)
+            if(matcher.matchesPartially()) {
                 String methodName = split[2]
                 String params = getParams(methodName, jar)
                 line = line + params
@@ -36,8 +45,11 @@ class AddParams extends DefaultTask {
         def params = GetParamMap.getMap(jar)
         def entry = params.find { it.key == methodName }
         for(int index : entry.value) {
-            String[] mSplit = methodName.split("_")
-            String methodId = mSplit[1]
+            String methodId
+            if(methodName.contains("func_")) {
+                String[] mSplit = methodName.split("_")
+                methodId = mSplit[1]
+            } else methodId = methodName
             String param = "\n\t\t" + index + " o " + "p_" + methodId + "_" + index
             paramList.add(param)
         }
