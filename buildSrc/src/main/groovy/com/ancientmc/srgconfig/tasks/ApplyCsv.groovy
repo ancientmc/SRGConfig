009bfa6e60831d9d5c06e9b8e2350f5b6877b5ed
moist-mason <67809772+moist-mason@users.noreply.github.com>
2023-02-19 19:11:28 -0600

    - more params - rewrote the csv application file, it's much, much faster now. I love Groovy.
    Signed-off-by: moist-mason <67809772+moist-mason@users.noreply.github.com>

--- buildSrc/src/main/groovy/com/ancientmc/srgconfig/tasks/ApplyCsv.groovy
+++ buildSrc/src/main/groovy/com/ancientmc/srgconfig/tasks/ApplyCsv.groovy
@@ -5,40 +5,46 @@ import com.opencsv.CSVReaderBuilder
 import com.opencsv.exceptions.CsvValidationException
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
-// TODO: Make this way faster because it's really slow and I hate it. It's probably because of my shitty older code, but it also
-// TODO: may be because of how Java parses text in general. Forge keeps things contained in ZIP files when parsing CSVs, and I'm beginning
-// TODO: to understand why. Rewrite will be soon, probably using Groovy stuff. Also, can you not do multi-line TODOs?
+import java.nio.file.Files
+
 class ApplyCsv extends DefaultTask {
     @InputFile File scriptFile
     @InputFile File csvFile
 
     @TaskAction
     void exec() throws CsvValidationException, IOException {
-        String srgName = ""
-        String mcpName = ""
-        String[] nextLine
-        int rowNumber = 0
-        CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withSkipLines(1).build()
+        List<String> lines = new ArrayList<>()
+        scriptFile.text.eachLine { line ->
+            def matches = getMap().findAll { line.contains(it.key) }
+            matches.each { line = line.replaceAll(it.key, it.value) }
+            lines.add(line)
+        }
 
-        while((nextLine = csvReader.readNext()) != null) {
-            rowNumber++
-            for (int i = 1; i < nextLine.length; i++) {
-                srgName = nextLine[0]
-                mcpName = nextLine[1]
-                Scanner scanner = new Scanner(scriptFile)
-                StringBuffer buffer = new StringBuffer()
-                while (scanner.hasNextLine()) {
-                    buffer.append(scanner.nextLine() + System.lineSeparator())
-                }
-                String contents = buffer.toString()
-                scanner.close()
-                contents = contents.replaceAll(srgName, mcpName)
-                FileWriter writer = new FileWriter(scriptFile)
-                writer.append(contents)
+        scriptFile.withWriter('UTF-8') { writer ->
+            lines.each {
+                writer.write(it + '\n')
                 writer.flush()
             }
         }
+    }
+
+    @Internal
+    Map<String, String> getMap() {
+        Map<String, String> map = new HashMap<>()
+        String[] nextLine
+        int rowNumber = 0
+        CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvFile)).withSkipLines(1).build()
+        while((nextLine = csvReader.readNext()) != null) {
+            rowNumber++
+            for (int i = 1; i < nextLine.length; i++) {
+                String srgName = nextLine[0]
+                String mcpName = nextLine[1]
+                map.put(srgName, mcpName)
+            }
+        }
+        return map
     }
 }
